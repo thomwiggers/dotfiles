c0ce5ace8fe2227c7f0d013b7739593934ab3f9a
Thom Wiggers <thom@thomwiggers.nl>
2012-09-06 23:08:20 +0300

    Update system/aliases.autorun.zsh

--- system/aliases.autorun.zsh
+++ system/aliases.autorun.zsh
@@ -3,10 +3,10 @@
 #   `brew install coreutils`
 if $(gls &>/dev/null)
 then
-  alias ls="gls -F --color"
-  alias l="gls -lAh --color"
-  alias ll="gls -l --color"
-  alias la='gls -A --color'
+  alias ls="ls -F --color"
+  alias l="ls -lAh --color"
+  alias ll="ls -l --color"
+  alias la='ls -A --color'
 fi
 
 alias pbcopy='xclip -selection clipboard'
